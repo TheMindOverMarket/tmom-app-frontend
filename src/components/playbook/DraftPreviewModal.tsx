0da0431dd54b5e1441bce451ab8d852ce283fcd6
Abhinav-Kotta <ask329@cornell.edu>
2026-04-17 18:41:23 -0400

    fixed build error

--- src/components/playbook/DraftPreviewModal.tsx
+++ src/components/playbook/DraftPreviewModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { X, CheckCircle2, ShieldAlert, Target, Zap, Activity } from 'lucide-react';
 
 interface DraftPreviewModalProps {
