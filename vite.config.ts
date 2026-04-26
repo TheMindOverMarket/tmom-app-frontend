57959768fc0f08ae536ccbdd4be27ab739f08c77
Abhinav-Kotta <ask329@cornell.edu>
2026-04-26 16:21:09 -0400

    updated for local and prod deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,34 +2,41 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+  
+  return {
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+        },
+      },
+      chunkSizeWarningLimit: 1000,
+    },
+    server: {
+      proxy: {
+        '/api/backend': {
+          target: isDev 
+            ? 'http://localhost:8000' 
+            : 'https://tmom-app-backend.onrender.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api\/backend/, ''),
+        },
+        '/api/engine': {
+          target: isDev 
+            ? 'http://localhost:8080' 
+            : 'https://rule-engine-rcg9.onrender.com',
+          changeOrigin: true,
+          ws: true,
+          rewrite: (path) => path.replace(/^\/api\/engine/, ''),
         },
       },
     },
-    chunkSizeWarningLimit: 1000,
-  },
-  server: {
-    proxy: {
-      // TODO Proxy for TMOM Backend (Rule Engine) to bypass CORS (move cors * to backend)
-      '/api/backend': {
-        target: 'https://tmom-app-backend.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/backend/, ''),
-      },
-      '/api/engine': {
-        target: 'https://rule-engine-rcg9.onrender.com',
-        changeOrigin: true,
-        ws: true,
-        rewrite: (path) => path.replace(/^\/api\/engine/, ''),
-      },
-    },
-  },
-})
+  };
+});
